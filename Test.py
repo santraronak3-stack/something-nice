248c1ba0292c1427b6f812cfc8f797d3942a76eb
ronakworld <santraronak3@gmail.com>
2026-09-02 21:07:11 +0530

    Enhance calculator GUI with error handling and quit option
    Refactored calculator GUI to improve error handling and added a quit button.

--- Test.py
+++ Test.py
@@ -1,18 +1,28 @@
 import tkinter
+# Variables
+f_color1 = "cyan"
+bg_color1 = "grey"; bg_color2 = "cyan"
 def main():
     win1 = tkinter.Tk()
     win1.title("Test")
     win1.geometry("500x300")
-    win1["bg"] = "grey"
+    win1["bg"] = bg_color1
     s1 = tkinter.Label(win1, text="A Simple eval()-based calculator", bg="grey",
-         fg="white",
+         fg=f_color1,
          font=("Consolas", 10)); s1.pack()
-    inp = tkinter.Entry(win1, bg="grey"); inp.pack()
-    a = tkinter.Label(win1, text="Result: ", bg="grey", font=("Consolas", 10)); a.pack()
-    def test1():
-        a.config(text="Result: " + str(eval(inp.get())))
-    b1 = tkinter.Button(win1, text="Calculate", command=test1, font=("Consolas", 10))
+    inp = tkinter.Entry(win1, bg=bg_color2); inp.pack()
+    a = tkinter.Label(win1, text="Result: ", bg="grey", font=("Consolas", 10), fg=f_color1); a.pack()
+    def calc_engine1():
+        try:
+            a.config(text="Result: " + str(eval(inp.get(), {"__builtins__": None})), fg=f_color1)
+        except Exception as e:
+            a.config(text="Result: Type a valid input", fg=f_color1)
+    b1 = tkinter.Button(win1, text="Calculate", command=calc_engine1, font=("Consolas", 10), bg=f_color1)
     b1.pack()
+    def leaver():
+        win1.destroy()
+    b2 = tkinter.Button(win1, text="Quit", command=leaver, font=("Consolas", 10), bg=f_color1)
+    b2.pack(pady=5)
     win1.mainloop()
 
 
