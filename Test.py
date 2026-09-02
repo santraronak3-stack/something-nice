0c3da5880bd73643ef99f8f7ac30154cb7d29c8d
ronakworld <santraronak3@gmail.com>
2026-09-02 21:04:18 +0530

    Add basic tkinter GUI calculator
    Implemented a simple GUI calculator using tkinter.

--- Test.py
+++ Test.py
@@ -1,3 +1,19 @@
-import tkinter as a
-h = a.Tk()
-print(h)
+import tkinter
+def main():
+    win1 = tkinter.Tk()
+    win1.title("Test")
+    win1.geometry("500x300")
+    win1["bg"] = "grey"
+    s1 = tkinter.Label(win1, text="A Simple eval()-based calculator", bg="grey",
+         fg="white",
+         font=("Consolas", 10)); s1.pack()
+    inp = tkinter.Entry(win1, bg="grey"); inp.pack()
+    a = tkinter.Label(win1, text="Result: ", bg="grey", font=("Consolas", 10)); a.pack()
+    def test1():
+        a.config(text="Result: " + str(eval(inp.get())))
+    b1 = tkinter.Button(win1, text="Calculate", command=test1, font=("Consolas", 10))
+    b1.pack()
+    win1.mainloop()
+
+
+main()
